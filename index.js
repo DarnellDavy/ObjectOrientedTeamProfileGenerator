0e24da6b45df908140c870cd407eddd4707459e2
Darnell Davy <darnelldavy@darnells-mbp.myfiosgateway.com>
2021-12-28 13:12:15 -0500

    updated java

--- index.js
+++ index.js
@@ -49,3 +49,55 @@ var baseQuestions = [
     }
     }
 ];
+
+var managerQuestions = [...baseQuestions];
+managerQuestions.push({
+        type: 'input',
+        name: 'officeNumber', 
+        message: 'Please enter employee office number.',
+        validate: value => {
+          if (value) {
+              return true;
+          } else {
+              console.log('You must enter an office number!')
+              return false;
+          }
+        }
+    })
+
+var engineerQuestions = [...baseQuestions];
+engineerQuestions.push({
+    type: 'input',
+    name: 'github', 
+    message: 'Please enter employee GitHub Repository.',
+    validate: value => {
+      if (value) {
+          return true;
+      } else {
+          console.log('You must enter a github repository!')
+          return false;
+      }
+    }
+});
+
+var internQuestions = [...baseQuestions];
+internQuestions.push({
+    type: 'input',
+    name: 'school', 
+    message: 'Please enter employee school.',
+    validate: value => {
+      if (value) {
+          return true;
+      } else {
+          console.log('You must enter a school!')
+          return false;
+      }
+    }
+});
+
+inquirer.prompt(
+    managerQuestions
+).then((answers) => {
+    groupMembers.push(new Manager(answers.name, answers.id, answers.email, answers.officeNumber));
+    promptForEmployee();
+});
